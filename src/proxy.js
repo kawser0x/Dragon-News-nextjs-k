1ca836e56bcc2048304093815878460230d97fbf
kawser0x <kawserswe@gmail.com>
2026-07-21 11:42:11 +0600

    proxy dns update

--- src/proxy.js
+++ src/proxy.js
@@ -1,3 +1,6 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import { NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
@@ -14,5 +17,5 @@ export async function proxy(request) {
 }
 
 export const config = {
-  matcher: ["/career","/news/:path*"],
+  matcher: ["/career", "/news/:path*"],
 };
